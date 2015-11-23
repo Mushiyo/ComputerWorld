6857bc59ea8b3f85c40270d342ffe31ae3976e30
Mushiyo <mushiyo@gmail.com>
2015-11-24 00:20:52 +0800

    1. Integrate day theme and night theme together (css) 2. modify the switch theme function from switch css to add/remove class

--- assets/js/switchTheme.js
+++ assets/js/switchTheme.js
@@ -1,13 +1,13 @@
 var isDay = true;
 function switchTheme() {
 	if (isDay){
-		document.getElementById('blogTheme').href = '/css/computerWorldNight.css';
+		$('body').addClass('night');
 		document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeEmacs.css';
 		document.getElementById('toggle').innerHTML = "ON";
 		isDay = false;
 		$.cookie('theme', 'night', {path: '/' });
 	} else {
-		document.getElementById('blogTheme').href = '/css/computerWorldDay.css';
+		$('body').removeClass('night');
 		document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeDefault.css';
 		document.getElementById('toggle').innerHTML = "OFF";
 		isDay = true;
@@ -20,10 +20,8 @@ $(function() {
 	var theme = $.cookie('theme');
 	
 	if (theme == 'day' && isDay == false) {
-		//isDay = true;
 		switchTheme();
 	} else if(theme == 'night' && isDay){
-		//isDay = false;
 		switchTheme();
 	}
 });
