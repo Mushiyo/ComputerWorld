b4b0cce2076bd1479dab63ca6d3e598cd87ab5ac
Mushiyo <mushiyo@gmail.com>
2015-11-23 22:22:15 +0800

    1. fix jQuery cookie function and making switched theme consisted between pages 2. enhanced isso post box in Night theme

--- assets/js/switchTheme.js
+++ assets/js/switchTheme.js
@@ -1,16 +1,29 @@
+var isDay = true;
+function switchTheme() {
+	if (isDay){
+		document.getElementById('blogTheme').href = '/css/computerWorldNight.css';
+		document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeEmacs.css';
+		document.getElementById('toggle').innerHTML = "ON";
+		isDay = false;
+		$.cookie('theme', 'night', {path: '/' });
+	} else {
+		document.getElementById('blogTheme').href = '/css/computerWorldDay.css';
+		document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeDefault.css';
+		document.getElementById('toggle').innerHTML = "OFF";
+		isDay = true;
+		$.cookie('theme', 'day', {path: '/' });
+	}	
+}
+
 $(function() {
-	var isDay = true;
-	document.getElementById('toggle').onclick = function () {
-		if (isDay){
-			document.getElementById('blogTheme').href = '/css/computerWorldNight.css';
-			document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeEmacs.css';
-			document.getElementById('toggle').innerHTML = "ON";
-			isDay = false;
-		} else {
-			document.getElementById('blogTheme').href = '/css/computerWorldDay.css';
-			document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeDefault.css';
-			document.getElementById('toggle').innerHTML = "OFF";
-			isDay = true;
-		}
-	};
+	$.cookie.defaults.path = '/';
+	var theme = $.cookie('theme');
+	
+	if (theme == 'day' && isDay == false) {
+		//isDay = true;
+		switchTheme();
+	} else if(theme == 'night' && isDay){
+		//isDay = false;
+		switchTheme();
+	}
 });
