ce0e46d6645b560d46060913817e60aed7fb645e
Mushiyo <mushiyo@gmail.com>
2015-11-24 21:03:42 +0800

    1. fixed night theme navbar color 2. made day theme into a class

--- js/switchTheme.js
+++ js/switchTheme.js
@@ -1,13 +1,13 @@
 var isDay = true;
 function switchTheme() {
 	if (isDay){
-		$('body').addClass('night');
+		$('body').removeClass('day').addClass('night');
 		document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeEmacs.css';
 		document.getElementById('toggle').innerHTML = "ON";
 		isDay = false;
 		$.cookie('theme', 'night', {path: '/' });
 	} else {
-		$('body').removeClass('night');
+		$('body').removeClass('night').addClass('day');
 		document.getElementById('shTheme').href = 'http://dl.dropbox.com/u/46484098/blog_script/syntaxhighlighter_3.0.83/styles/shThemeDefault.css';
 		document.getElementById('toggle').innerHTML = "OFF";
 		isDay = true;
@@ -20,8 +20,10 @@ $(function() {
 	var theme = $.cookie('theme');
 	
 	if (theme == 'day' && isDay == false) {
+		//isDay = true;
 		switchTheme();
 	} else if(theme == 'night' && isDay){
+		//isDay = false;
 		switchTheme();
 	}
 });
